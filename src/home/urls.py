77e37218bce42b7b4afd687bce544f5b6fcfdcde
nightman19 <omahnuru2010@gmail.com>
2025-12-25 07:25:50 +0000

    Initial commit: Django 5.2.9 Inspirobot project with Tailwind and HTMX

---
+++ src/home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views 
+
+appname = "home"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("inspirobot/", views.quotes_view, name="inspirobot")
+]
+
+
+
