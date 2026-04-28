efb23eda477ecfaa8a1952423bf46465c92a9da1
nightman19 <omahnuru2010@gmail.com>
2026-04-28 17:27:53 +0000

    Clean repo, fix Procfile for production, move requirements to root

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn src.inspirobot.wsgi:application
--- src/inspirobot/settings/prod.py
+++ src/inspirobot/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = [".onrender.com"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", ".onrender.com").split(",")
 
 # Security settings
 CSRF_COOKIE_SECURE = True
