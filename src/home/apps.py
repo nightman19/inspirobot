57d0149ab75d943cbd9e998b80509a2d1d0ed32d
nightman19 <omahnuru2010@gmail.com>
2026-06-05 23:06:07 +0000

    Temporal auth fixing

--- src/home/apps.py
+++ src/home/apps.py
@@ -2,4 +2,16 @@ from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    
     name = 'home'
+
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        if not User.objects.filter(username="nuru").exists():
+            User.objectd.create_user(
+                username="nuru",
+                password="test1234"
+            )
